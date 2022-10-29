abdc2da02c4c1889da70f3ceb24e3919c0f4e2a6
ted-jv <wkejrdldls@gmail.com>
2022-10-29 23:55:58 +0900

    Feat: 추가 - Swagger Api 명세서 status 내용

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,12 +1,15 @@
 import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { JoinRequestDtd } from './dto/join.request.dto';
-import { ApiOperation } from '@nestjs/swagger';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
+import { User } from 'src/common/decorators/user.decorator';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {} // 다른 곳에서 값을 가져와서 아래 사용했으면 constructor에 꼭 초기값 넣어줄것 )
 
+  @ApiResponse({ status: 200, description: '성공' })
+  @ApiResponse({ status: 500, description: '에러' })
   @ApiOperation({ summary: '회원가입' })
   @Post() // 회원가입
   createSignUp(@Body() data: JoinRequestDtd) {
@@ -19,8 +22,8 @@ export class UsersController {
   // }
 
   @Get()
-  findAll() {
-    return this.usersService.findAll();
+  getUsers(@User() user) {
+    return user;
   }
 
   @Get(':id')
